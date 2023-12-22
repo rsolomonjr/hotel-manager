6429c5f1199fa6f6a1060c613437d9de6ded4f96
rsolomon <rsolomon@webmd.net>
2023-12-22 17:36:37 -0500

    Hotel Mgmt App - Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import {FaUserCircle} from 'react-icons/fa';
+
+export const Header = () => {
+  return (
+    <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between">
+        <div className="flex items-center w-full md:2/3">
+            <Link href="/" className="font-black text-[#F27405]">
+                Hotelzzz 
+            </Link>
+            <ul className="flex items-center ml-5">
+                <li className="flex items-center">
+                    <Link href="/auth">
+                        <FaUserCircle className="cursor-pointer" />
+                    </Link>
+                </li>
+            </ul>
+        </div>
+
+        <ul></ul>
+    </header>
+  );
+};
